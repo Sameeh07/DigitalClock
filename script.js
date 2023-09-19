edb26af37956cec24f7f36d54973d8407ebb9cda
ABDUL SAMEEH K <124634392+Sameeh07@users.noreply.github.com>
2023-09-19 19:32:21 +0530

    script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
             hour = hour < 10 ? "0" + hour : hour;
             min = min < 10 ? "0" + min : min; 
             sec = sec < 10 ? "0" + sec : sec; 
-            let currentTime = hour + ":" + min + ":" + sec + am_pm
+            let currentTime = hour + ":" + min + ":" + sec + " " + am_pm
             document.getElementById("clock").innerHTML = currentTime;
             }
             showTime();
